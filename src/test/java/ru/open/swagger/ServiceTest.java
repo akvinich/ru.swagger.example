71604caa486efd4c4a4572c0d43821f90ad7094e
vitali <vital1234mail@gmail.com>
2020-08-11 00:14:10 +0300

    Added smoke.xml

--- src/test/java/ru/open/swagger/ServiceTest.java
+++ src/test/java/ru/open/swagger/ServiceTest.java
@@ -27,6 +27,4 @@ public class ServiceTest {
         MDC.put("TestID", stamp);
     }
 
-
-
 }
--- src/test/java/ru/open/swagger/UsersTest.java
+++ src/test/java/ru/open/swagger/UsersTest.java
@@ -1,18 +1,28 @@
 package ru.open.swagger;
 
+import com.google.common.primitives.Ints;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Severity;
 import io.qameta.allure.Story;
+import io.restassured.RestAssured;
+import org.slf4j.MDC;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.open.swagger.dto.DoRegisterPojo;
 import ru.open.swagger.steps.Steps;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Optional;
 
 import static io.qameta.allure.SeverityLevel.CRITICAL;
 
-public class UsersTest extends ServiceTest {
+public class UsersTest {
+
+    static {
+        RestAssured.useRelaxedHTTPSValidation();
+    }
 
     public final static String BASE_URL = "http://users.bugred.ru/tasks/rest";
     public final static String ENDPOINT_DO_REGISTER = "/doRegister";
@@ -24,6 +34,20 @@ public class UsersTest extends ServiceTest {
 
     private Steps steps = new Steps(BASE_URL);
     private DoRegisterPojo doRegisterPojo = new DoRegisterPojo();
+
+    public UsersTest() {}
+
+    @BeforeMethod(alwaysRun = true)
+    protected void setupLogStamp(Method method) {
+        Optional<Story> allureStory = Optional.ofNullable(method.getAnnotation(Story.class));
+        Integer testIdValue = allureStory
+                .map(Story::value)
+                .map(value -> value.split("\\s")[0])
+                .map(Ints::tryParse)
+                .orElse(null);
+        String stamp = String.format("TestID=%d", testIdValue);
+        MDC.put("TestID", stamp);
+    }
 
     @Epic("Smoke")
     @Story("Users-100")
